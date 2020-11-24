747c9f932751604ab2702f8794bc265179b9c9e8
Filipe Martins <f.martins@runtime-revolution.com>
2020-11-24 13:54:14 +0000

    testing all together

--- Package.swift
+++ Package.swift
@@ -16,124 +16,26 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "KSCrash/Installations",
-            path: "Source/KSCrash/Installations",
+            name: "KSCrash",
+            path: "Source/KSCrash",
             publicHeadersPath: ".",
             cxxSettings: [
-                .headerSearchPath("../Recording"),
-                .headerSearchPath("../Recording/Monitors"),
-                .headerSearchPath("../Recording/Tools"),
-                .headerSearchPath("../Reporting/Filters"),
-                .headerSearchPath("../Reporting/Sinks"),
-                .headerSearchPath("../Reporting/Tools"),
-            ]
-        ),
-        .target(
-            name: "KSCrash/Recording",
-            path: "Source/KSCrash/Recording",
-            exclude: [
-                "Monitors",
-                "Tools"
-            ],
-            publicHeadersPath: ".",
-            cxxSettings: [
+                .headerSearchPath("Recording"),
+                .headerSearchPath("Recording/Monitors"),
+                .headerSearchPath("Recording/Tools"),
+                .headerSearchPath("Reporting/Filters"),
+                .headerSearchPath("Reporting/Sinks"),
+                .headerSearchPath("Reporting/Tools"),
                 .headerSearchPath("Tools"),
-                .headerSearchPath("Monitors"),
-                .headerSearchPath("../Reporting/Filters")
-            ]
-        ),
-        .target(
-            name: "KSCrash/Recording/Monitors",
-            path: "Source/KSCrash/Recording/Monitors",
-            publicHeadersPath: ".",
-            cxxSettings: [
+                .headerSearchPath("Recording/Monitors"),
                 .define("GCC_ENABLE_CPP_EXCEPTIONS", to: "YES"),
-                .headerSearchPath(".."),
-                .headerSearchPath("../Tools"),
-                .headerSearchPath("../../Reporting/Filters")
+                .headerSearchPath("Recording"),
+                .headerSearchPath("swift"),
+                .headerSearchPath("swift/Basic"),
+                .headerSearchPath("llvm/ADT"),
+                .headerSearchPath("llvm/Support"),
+                .headerSearchPath("llvm/Config")
             ]
-        ),
-        .target(
-            name: "KSCrash/Recording/Tools",
-            path: "Source/KSCrash/Recording/Tools",
-            publicHeadersPath: ".",
-            cxxSettings: [
-                .headerSearchPath(".."),
-                .headerSearchPath("../../swift"),
-                .headerSearchPath("../../swift/Basic"),
-                .headerSearchPath("../../llvm/ADT"),
-                .headerSearchPath("../../llvm/Support"),
-                .headerSearchPath("../../llvm/Config")
-            ]
-        ),
-        .target(
-            name: "KSCrash/Reporting/Filters",
-            path: "Source/KSCrash/Reporting/Filters",
-            exclude: [
-                "Tools"
-            ],
-            publicHeadersPath: ".",
-            cxxSettings: [
-                .headerSearchPath("Tools"),
-                .headerSearchPath("../../Recording"),
-                .headerSearchPath("../../Recording/Monitors"),
-                .headerSearchPath("../../Recording/Tools")
-            ]
-        ),
-        .target(
-            name: "KSCrash/Reporting/Filters/Tools",
-            path: "Source/KSCrash/Reporting/Filters/Tools",
-            publicHeadersPath: ".",
-            cxxSettings: [
-                .headerSearchPath("../../../Recording/Tools")
-            ]
-        ),
-        .target(
-            name: "KSCrash/Reporting/Tools",
-            path: "Source/KSCrash/Reporting/Tools",
-            publicHeadersPath: ".",
-            cxxSettings: [
-                .headerSearchPath("../../Recording"),
-                .headerSearchPath("../../Recording/Tools")
-            ]
-        ),
-        .target(
-            name: "KSCrash/Reporting/Sinks",
-            path: "Source/KSCrash/Reporting/Sinks",
-            publicHeadersPath: ".",
-            cxxSettings: [
-                .headerSearchPath("../Filters"),
-                .headerSearchPath("../Filters/Tools"),
-                .headerSearchPath("../Tools"),
-                .headerSearchPath("../../Recording"),
-                .headerSearchPath("../../Recording/Tools"),
-                .headerSearchPath("../../Recording/Monitors")
-            ]
-        ),
-        .target(
-            name: "KSCrash/swift/Basic",
-            path: "Source/KSCrash/swift/Basic",
-            publicHeadersPath: ".",
-            cxxSettings: [
-                .headerSearchPath(".."),
-                .headerSearchPath("../../llvm/ADT"),
-                .headerSearchPath("../../llvm/Config"),
-                .headerSearchPath("../../llvm/Support")
-            ]
-        ),
-        .target(name: "KSCrash",
-                dependencies: [
-                    .target(name: "KSCrash/Installations"),
-                    .target(name: "KSCrash/Recording"),
-                    .target(name: "KSCrash/Recording/Monitors"),
-                    .target(name: "KSCrash/Recording/Tools"),
-                    .target(name: "KSCrash/Reporting/Filters"),
-                    .target(name: "KSCrash/Reporting/Filters/Tools"),
-                    .target(name: "KSCrash/Reporting/Tools"),
-                    .target(name: "KSCrash/Reporting/Sinks"),
-                    .target(name: "KSCrash/swift/Basic")
-                ],
-                path: "Wrapper"
         )
     ],
     cxxLanguageStandard: .gnucxx11
